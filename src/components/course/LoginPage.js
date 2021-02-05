3ea43f543184213c96aef7afe80d5ef8e89bc34f
Dhanushroyal123 <dhanushroyal516@gmail.com>
2021-02-05 21:14:45 +0530

    commit

--- src/components/course/LoginPage.js
+++ src/components/course/LoginPage.js
@@ -38,7 +38,6 @@ const LoginForm = () => {
       .post('https://dhanush-test-app.herokuapp.com/user/validate', user)
       .then((res) => {
         if (res.data.value < 300) {
-          username = res.data.username
           history.push('/courses')
         } else {
           setShow(true)
