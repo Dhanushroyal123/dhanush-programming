5b762def849c93868807adcfe110df775e53050a
Dhanushroyal123 <dhanushroyal516@gmail.com>
2021-02-06 08:48:52 +0530

    'commit'

--- src/components/course/body.js
+++ src/components/course/body.js
@@ -2,6 +2,14 @@ import { data } from './data'
 import { useEffect } from 'react'
 
 const Coursebody = () => {
+  useEffect(() => {
+    window.onload = function () {
+      if (!window.location.hash) {
+        window.location = window.location + '#loaded'
+        window.location.reload()
+      }
+    }
+  }, [])
   return (
     <>
       <div className='course-body'>
